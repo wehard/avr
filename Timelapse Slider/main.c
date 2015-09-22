58dade9e0ec837d8b1d9e69fcff687c952cf7e37
Willehard Korander <willehard@gmail.com>
2015-09-22 16:05:16 +0300

    Trying to get rotary encoder to work...

--- Timelapse Slider/main.c
+++ Timelapse Slider/main.c
@@ -4,18 +4,30 @@
 #include <stdlib.h>
 #include "MrLCD.h"
 
+#define ENC_PIN		PINA
+#define ENC_PIN1 	PA0
+#define ENC_PIN2 	PA1
+#define ENC_BTN		PA2
+
 uint8_t read_gray_code_from_encoder(void)
 {
 	uint8_t val = 0;
-	if(!bit_is_clear(PIND, PD2))
+	if(!bit_is_clear(ENC_PIN, ENC_PIN1))
 	{
 		val |= (1<<1);
 	}
-	if(!bit_is_clear(PIND, PD3))
+	if(!bit_is_clear(ENC_PIN, ENC_PIN1))
 	{
 		val |= (1<<0);
 	}
 	return val;
+}
+
+void initialize_rotary_encoder(void)
+{
+	DDRA &=~ (1 << ENC_PIN1);				/* Set pins as input */
+	DDRA &=~ (1 << ENC_PIN2);        
+	PORTA |= (1 << ENC_PIN1)|(1 << ENC_PIN2); /* Set pins pullup enabled */
 }
 
 void LCD_Splash(void)
@@ -32,19 +44,48 @@ int main(void)
 	
 	uint8_t val = 0, val_tmp = 0;
 	
+	initialize_rotary_encoder();
+	
 	char counterString[4];
 	int count = 0;
 	while(1)
 	{
+		val_tmp = read_gray_code_from_encoder();
+		
+		if(val != val_tmp)
+		{
+			if((val==3 && val_tmp == 1) || (val==0 && val_tmp==2))
+			{
+				count++;
+			}
+			else if((val==2 && val_tmp==0) || (val==1 && val_tmp==3))
+			{
+				count--;
+			}
+			val = val_tmp;
+			itoa(count, counterString, 10);
+		
+		Goto_LCD_Location(1,1);
+		Send_A_String(counterString);
+		}
+		
+		_delay_ms(1);
+		
+		
+		
+		/*
 		itoa(count, counterString, 10);
 		Goto_LCD_Location(1, 2);
 		Send_A_String("Count = ");
 		Send_A_String(counterString);
 		count++;
 		_delay_ms(1000);
+		*/
 	}
-	
-	void RunTimeLapseProgram()
+	return 0;
+}
+
+void RunTimeLapseProgram()
 	{
 		//Calculate how many steps are needed to move at selected speed
 		//Send required amount of pulses to motor controller
@@ -60,4 +101,3 @@ int main(void)
 		//While loop that moves motor until left limit is reached
 		//Set flag for motor ready
 	}
-}
